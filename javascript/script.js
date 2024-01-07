e70fe4a51116cc5a532c580e2bb9b777901a45bf
Sᴀʀᴛʜᴀᴋぷ / <113484765+sarrthak008@users.noreply.github.com>
2024-01-07 18:06:27 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+
+function resposiveNav() {
+
+    let hamb = document.querySelector("#nav-img");
+    let resNav = document.querySelector("#res-nav");
+    let flag = 0;
+  
+    hamb.addEventListener("click", () => {
+      if (flag == 0) {
+        resNav.style.top = 10 + 'vh';
+        flag = 1;
+      } else {
+        resNav.style.top = -100 + 'vh';
+        flag = 0;
+      }
+  
+    })
+  }
+  resposiveNav();
+  
+
+setTimeout(() => {
+    var first =document.querySelector('#first')
+    first.style.top=-200+'vh';
+    
+}, 6000);
